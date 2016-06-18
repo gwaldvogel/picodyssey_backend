4f3134ebd4fd98ebbc80ac03a71f2cfeebbf3b57
Blatts01 <stefan.blattmann@gmx.de>
2016-06-18 19:20:06 +0200

    update Test script

--- FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/configureDatabase.py
+++ FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/configureDatabase.py
@@ -56,10 +56,107 @@ Anna = {
  
 names =  [Ali,Memed,Sven,Hans,Lisa,Anna]
 for people in names:
-    res = requests.post('http://127.0.0.1:80/addUser', json=people)
+    res = requests.post('http://localhost:80/addUser', json=people)
     if res.ok:
-        print res.json()
-    
- 
+        print(res.json())
 
+Hauptbahnhof = {
+   "placeId" : "0815",
+   "userId" : "4711",
+   "date": "205",
+   "name": "Hauptbahnhof",
+   "geoN": ["20"],
+   "geoE": ["40"],
+   "image": "WP_20160618_16_35_44_Pro_LI.jpg",
+   "thumbnail": "",
+   "rate": "4.5",
+   "numberOfRates": "20",
+   "description": "Hauptbahnhof Freiburg",
+   "city": "Freiburg",
+   "type" : "place"
+}     
+ 
+Hermann = {
+   "placeId" : "2569",
+   "userId" : "3597",
+   "date": "205",
+   "name": "Hermann Cafe",
+   "geoN": ["20"],
+   "geoE": ["40"],
+   "image": "WP_20160618_16_36_12_Pro_LI.jpg",
+   "thumbnail": "",
+   "rate": "4.5",
+   "numberOfRates": "20",
+   "description": "Cooles Cafe",
+   "city": "Freiburg",
+   "type" : "place"
+}    
     
+BlaueBrücke = {
+   "placeId" : "7777",
+   "userId" : "5897",
+   "date": "205",
+   "name": "BlaueBrücke",
+   "geoN": ["20"],
+   "geoE": ["40"],
+   "image": "WP_20160618_16_37_01_Pro_LI.jpg",
+   "thumbnail": "",
+   "rate": "4.5",
+   "numberOfRates": "20",
+   "description": "Blaue Brücke in  freiburg",
+   "city": "Freiburg",
+   "type" : "place"
+}   
+
+Theater = {
+   "placeId" : "8888",
+   "userId" : "4478",
+   "date": "205",
+   "name": "Theater",
+   "geoN": ["20"],
+   "geoE": ["40"],
+   "image": "hello.jpg",
+   "thumbnail": "hello_thumbnail.jpg",
+   "rate": "4.5",
+   "numberOfRates": "20",
+   "description": "Theater Haus freiburg",
+   "city": "Freiburg",
+   "type" : "place"
+}   
+
+UniBib = {
+   "placeId" : "1382",
+   "userId" : "2259",
+   "date": "205",
+   "name": "UniBib",
+   "geoN": ["20"],
+   "geoE": ["40"],
+   "image": "WP_20160618_16_42_45_Pro_LI.jpg",
+   "thumbnail": "",
+   "rate": "4.5",
+   "numberOfRates": "20",
+   "description": "UniBib freiburg",
+   "city": "Freiburg",
+   "type" : "place"
+}   
+
+Bertholdsbrunnen = {
+   "placeId" : "1382",
+   "userId" : "2259",
+   "date": "205",
+   "name": "Bertholdsbrunnen",
+   "geoN": ["20"],
+   "geoE": ["40"],
+   "image": "WP_20160618_16_45_53_Pro_LI.jpg",
+   "thumbnail": "",
+   "rate": "4.5",
+   "numberOfRates": "20",
+   "description": "Bertholdsbrunnen freiburg",
+   "city": "Freiburg",
+   "type" : "place"
+}   
+places =  [UniBib,Theater,BlaueBrücke,Hermann,Hauptbahnhof,Bertholdsbrunnen]
+for place in places:
+    res = requests.post('http://localhost:80/addPlace', json=place)
+    if res.ok:
+        print(res.json())
